dd758e7acb4c916ea2f580fe3a060462e72145c6
Roohan Nawaz <roohan.nawaz159@gmail.com>
2025-05-27 15:58:55 +0500

    start

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Register Route
+Route::get('register', function () {
+    return view('register');
+});
+Route::post('register', [RegistrationController::class, 'register']);
+
+//Login Route
+Route::get('login', function () {
+    return view('login');
+});
+Route::post('login', [AuthController::class, 'login']);
+
+//Logout Route
+Route::get('logout', function () {
+    return view('logout');
+});
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
