297e448e982369e29ec8feceec01b9df32593587
Roohan Nawaz <roohan.nawaz159@gmail.com>
2025-05-30 20:39:41 +0500

    basic structure for auth api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(["success" => true, "message" => "User created successfully"], 200);
+    }
+
+    public function login(Request $request){
+        try{
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            $credentials = $request->only('email', 'password');
+
+            if(!$token = JWTAuth::attempt($credentials)){
+                return response()->json(["success" => false, "message" => "Invalid Credentials"], 401);
+            }
+
+            return response()->json(['token' => $token]);
+        }catch(JWTException $e){
+            return response()->json(["success" => false, "message" => $e->getMessage()], 500);
+        }
+    }
+
+    public function logout(Request $request){
+
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(["success" => true, "message" => "Logged out successfully"], 200);
+    }
+
+    public function me(){
+        try{
+            return response()->json(JWTAuth::User());
+        }catch(\Tymon\JWTAuth\Exceptions\JWTException $e){
+            return response()->json(["success" => false, "message" => $e->getMessage()]);
+        }
+
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+route::get('/ping',function(){
+    return ['message' => 'pong'];
+})->middleware('auth:api');
+
+//Login Route
+Route::post('/login', [AuthController::class, 'login']);
+
+//Register Route
+Route::post('/register', [AuthController::class, 'register']);
+
+route::group(['middleware' => 'auth:api'], function($route){
+//Logout Route
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+//get authenticated user
+    Route::post('/me', [AuthController::class, 'me']);
+});
+
+
+
