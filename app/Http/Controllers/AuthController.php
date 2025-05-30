031fb20d708c671f941c0ef7def443b1c828f38e
Roohan Nawaz <roohan.nawaz159@gmail.com>
2025-05-30 21:14:25 +0500

    done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
     public function login(Request $request){
         try{
             $request->validate([
-                'email' => 'required|emai',
+                'email' => 'required|email',
                 'password' => 'required',
             ]);
 
